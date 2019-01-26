435c9fed930bcd5915354ee8d95cd7a56465a144
Ian Torres <iantorres@outlook.com>
2019-01-25 23:54:12 -0300

    Handlers comments added.

--- handlers.js
+++ handlers.js
@@ -1,12 +1,26 @@
+/**
+ * Handlers
+ */
 module.exports = {
+    /**
+     * This handler will be executed when client make connection with the server
+     */
     openHandler: function () {
         console.log("Client create a new connection...");
 
         this.send("Thanks for welcome...", {}, () => {});
     },
+    /**
+     * This handler will be executed when client receive a message from the server
+     * @param data
+     */
     messageHandler: function (data) {
         console.log("Client receive a new message: ", data);
     },
+    /**
+     * This handler will be executed when client emmit an error event
+     * @param err
+     */
     errorHandler: function (err) {
         switch (err.code) {
             case "ECONNREFUSED":
@@ -14,6 +28,9 @@ module.exports = {
                 break;
         }
     },
+    /**
+     * This handler will be executed when client emmit an close event
+     */
     closeHandler: function () {
         console.log("Client closed...");
     }
